865d56ab6545abe063d2a946f37cdcf00c9e9977
Norbert Gyulai <gyulai.norbert@gmail.com>
2020-09-25 19:05:56 +0200

    20ig

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            for(int i=0, i<10, i++)
+            for(int i=0; i<20; i++)
             {
                 Console.WriteLine(i);
             }
